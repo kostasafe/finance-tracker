a2dc906575cc27fe77e1fedd2ca941647586645d
kostasafe <22775121+kostasafe@users.noreply.github.com>
2025-11-24 20:23:28 +0200

    add pydantic schemas for user and token"

---
+++ app/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, EmailStr, constr
+from typing import Optional
+from datetime import date
+
+class UserCreate(BaseModel):
+    username: constr(min_length=3, max_length=64)
+    email: EmailStr
+    password: constr(min_length=6)
+
+
+class UserOut(BaseModel):
+    id:int
+    username: str
+    email: str
+
+
+    class Config:
+        orm_mode = True
+    
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
