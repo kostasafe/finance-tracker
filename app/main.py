6eadc02ec2a38cfa51889c04e9fdd5edf095421a
kostasafe <22775121+kostasafe@users.noreply.github.com>
2025-11-26 20:06:50 +0200

    include auth router in main app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
 
-app = FastAPI()
+load_dotenv()
+
+app = FastAPI(title="Finance Tracker")
+
+#import and include routers
+from app.routes import auth as auth_router #relative import of the file you created
+app.include_router(auth_router.router)
 
 @app.get("/")
 def root():
